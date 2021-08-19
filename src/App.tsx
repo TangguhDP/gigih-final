dd5d0417ff0f9d795c0a22f892481e6b6221b4af
Tangguh Pramono <tangguhmyname@gmail.com>
2021-08-19 22:32:08 +0700

    initiate routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/create-playlist">
+          <h1>Playlist</h1>
+          <Link to="/">Pindah</Link>
+        </Route>
+        <Route exact path="/">
+          <h1>Home</h1>
+          <Link to="/create-playlist">Pindah</Link>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
